123c988a46a469c2470b419d8eda0eac5721248d
MrYogesh0709 <yogeshvanzara98@gmail.com>
2024-03-22 19:06:02 +0530

    courses added

--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import { getCourses } from '@/db/queris';
+import { List } from './List';
 
-const CoursesPage = () => {
-  return <div>CoursesPage</div>;
+const CoursesPage = async () => {
+  const courses = await getCourses();
+  return (
+    <div className="mx-auto h-full max-w-[912px] px-3">
+      <h1 className="text-2xl font-bold text-neutral-700">Language Courses</h1>
+      <List courses={courses} activeCourseId={1} />
+    </div>
+  );
 };
 
 export default CoursesPage;
